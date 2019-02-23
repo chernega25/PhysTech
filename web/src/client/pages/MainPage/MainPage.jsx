d9b509a7bb62d5910db9a364df1f95e6a157375e
e.denisova <elin@MacBook-Pro-Elina.local>
2019-02-23 15:40:00 +0300

    add platform-ui

--- web/src/client/pages/MainPage/MainPage.jsx
+++ web/src/client/pages/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './MainPage.css';
+import Button from '@platform-ui/button';
 
 const MainPage = () => (
     <div className={styles.root}>
         <div> Main </div>
+        <Button>Hi</Button>
     </div>
 );
 
