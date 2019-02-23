f72764d77cbe34ca6f848544eea9a1791abb4e62
Vasiliy Morkovkin <1istoobig@gmail.com>
2019-02-23 23:19:30 +0300

    file loading

--- back/src/main/scala/Main.scala
+++ back/src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import com.twitter.finagle.Http
 import com.twitter.util.Await
 import io.finch._
 import monix.eval.Task
-import phystech.services.ModelsService
+import phystech.services.{ModelsService, TestingService}
 import io.circe.generic.auto._
 import io.finch.circe._
 import phystech.storage.mongo.MongoBase
@@ -12,6 +12,6 @@ object Main extends App with Endpoint.Module[Task] {
 
   implicit val mongo = new MongoBase("mongodb://phystech:phystech@209.250.236.189", "phystech")
   val modelsService = new ModelsService().combine(Bootstrap)
-
-  Await.ready(Http.server.serve(":8080", modelsService.toService))
+  val testingService = new TestingService().combine(modelsService)
+  Await.ready(Http.server.serve(":8080", testingService.toService))
 }
--- back/src/main/scala/phystech/services/TestingService.scala
+++ back/src/main/scala/phystech/services/TestingService.scala
@@ -1,5 +1,7 @@
 package phystech.services
 
+import java.nio.charset.Charset
+
 import io.finch.Endpoint
 import monix.eval.Task
 import monix.execution.Scheduler
@@ -7,7 +9,58 @@ import io.finch.{BadRequest, Ok}
 import io.finch._
 import com.twitter.io.Buf
 import com.twitter.concurrent.AsyncStream
+import io.finch.{BadRequest, Ok}
+import io.finch._
+import monix.eval.Task
+import monix.execution.Scheduler
+import phystech.responses.{
+  ChangeModelResponse,
+  ModelSummary,
+  NewModelResponse,
+  VariableNameWrapper
+}
+import io.circe.generic.auto._
+import io.finch.Decode.Dispatchable
+import io.finch.circe._
+import phystech.data.{Model, ModelVariable, Variable}
+import phystech.requests.{ChangeModelRequest, NewModelRequest}
+import phystech.storage.mongo.MongoBase
+import shapeless.HList
 
-class TestingService(implicit scheduler: Scheduler) extends Endpoint.Module[Task] {
-  final def testFirstStage: Endpoint[Task, AsyncStream[Buf]] = ???
+class TestingService(implicit scheduler: Scheduler)
+    extends Endpoint.Module[Task] {
+
+  implicit val csvDecoder: Dispatchable[String, Application.Csv] =
+    (ct: String, b: Buf, cs: Charset) => {
+      val arr: Array[Byte] = Array.fill(b.length)(0)
+      b.write(arr, 0)
+      Right(
+        arr
+          .map(_.toChar)
+          .mkString("")
+          .split("\n")
+          .drop(4)
+          .dropRight(2)
+          .mkString("\n"))
+    }
+
+  final def testFirstStage: Endpoint[Task, String] =
+    post("testFirstStage" :: body[String, Application.Csv]) {
+      body: String =>
+        println(body)
+        Ok(body)
+          .withHeader("Content-Disposition",
+                      "form-data; name=test; filename=test.csv")
+          .withHeader("Content-Type", "text/csv")
+    }
+
+  final def combine[ES <: HList, CTS <: HList](bootstrap: Bootstrap[ES, CTS]) =
+    Bootstrap
+      .serve[Text.Plain](
+        (testFirstStage) handle {
+          case e: Exception =>
+            println(s"Error: $e")
+            BadRequest(e)
+        }
+      )
 }
