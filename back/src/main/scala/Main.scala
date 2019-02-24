8ba7dea483281a63814b94687a70086c8375f8d2
valentiay <valentiay@yandex.ru>
2019-02-24 04:05:03 +0300

    CalculationService cache

--- back/src/main/scala/Main.scala
+++ back/src/main/scala/Main.scala
@@ -8,8 +8,12 @@ import phystech.services.{CalculationService, ModelsService, TestingService}
 import io.circe.generic.auto._
 import io.finch.circe._
 import phystech.storage.mongo.MongoBase
-import ch.qos.logback.classic.{Level,Logger}
+import ch.qos.logback.classic.{Level, Logger}
+import com.crobox.clickhouse.ClickhouseClient
+import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
+import phystech.storage.clickhouse.ClickHouseBase
+import scala.concurrent.duration._
 
 object Main extends App with Endpoint.Module[Task]  {
   val logger = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).asInstanceOf[Logger].setLevel(Level.INFO)
@@ -17,9 +21,11 @@ object Main extends App with Endpoint.Module[Task]  {
     case e: Exception => Json.obj("message" -> Json.fromString(e.getMessage))
   })
 
-  implicit val mongo = new MongoBase("mongodb://phystech:phystech@209.250.236.189", "phystech")
+  implicit val mongo: MongoBase = new MongoBase("mongodb://phystech:phystech@209.250.236.189", "phystech")
+  val clickhouseConfig = ConfigFactory.load("clickhouse.conf")
+  implicit val clickhouseClient: ClickhouseClient = ClickHouseBase.init(clickhouseConfig, "phystech")
   val modelsService = new ModelsService()
   val testingService = new TestingService()
-  val calculationService = new CalculationService()
+  val calculationService = CalculationService.mkCalculationService.runSyncUnsafe() // FIXME
   Await.ready(Http.server.serve(":8080", modelsService.combine(testingService.combine(calculationService.combine(Bootstrap))).toService))
 }
--- back/src/main/scala/phystech/services/CalculationService.scala
+++ back/src/main/scala/phystech/services/CalculationService.scala
@@ -1,5 +1,7 @@
 package phystech.services
 
+import java.time.Instant
+
 import io.finch.Endpoint
 import monix.eval.Task
 import monix.execution.Scheduler
@@ -12,17 +14,36 @@ import monix.catnap.MVar
 import phystech.calculate.CalculateModel
 import phystech.data.Model
 import phystech.responses.CalculateModelResponse
+import phystech.services.CalculationService.CacheEntry
 import shapeless.HList
 
-class CalculationService(implicit mongo: MongoBase, scheduler: Scheduler) extends Endpoint.Module[Task] {
+class CalculationService(private val mvar: MVar[Task, Map[String, CacheEntry]])(implicit mongo: MongoBase, scheduler: Scheduler) extends Endpoint.Module[Task] {
 
-  private def getModel(modelId: String): Task[Model] = ???
+  private val cacheTtlSeconds = 1000
+
+  private def updateCache(parentId: String): Task[Model] = for {
+      currentModelId <- mongo.getCurrentModel(parentId).map(_.currentModelId)
+      model <- mongo.getModel(currentModelId)
+      cache <- mvar.take
+      _ <- mvar.put(cache.updated(parentId, CacheEntry(model, Instant.now.plusSeconds(cacheTtlSeconds))))
+    } yield model
+
+  private def getModel(parentId: String): Task[Model] = for {
+    cache <- mvar.read
+    model <- cache.get(parentId) match {
+        case Some(CacheEntry(model, bestBefore)) if bestBefore.isAfter(Instant.now()) =>
+          println("CalculationService: cache hit")
+          Task.pure(model)
+        case _ =>
+          println("CalculationService: cache miss")
+          updateCache(parentId)
+      }
+  } yield model
 
   def calculateModel: Endpoint[Task, CalculateModelResponse] =
     post("calculateModel" :: jsonBody[CalculateModelRequest]) { body: CalculateModelRequest =>
       for {
-        currentModelId <- mongo.getCurrentModel(body.parentModelId).map(_.currentModelId)
-        model <- mongo.getModel(currentModelId)
+        model <- getModel(body.parentModelId)
         result = CalculateModel.apply(model, body.variableList.map(v => v.variableName -> v.variableValue).toMap)
         missingFields = model.variableList.map(_.variableName).diff(body.variableList.map(_.variableName))
       } yield Ok(CalculateModelResponse(result, missingFields))
@@ -36,4 +57,13 @@ class CalculationService(implicit mongo: MongoBase, scheduler: Scheduler) extend
         BadRequest(e)
     }
   )
+}
+
+object CalculationService {
+  case class CacheEntry(model: Model, bestBefore: Instant)
+
+  def mkCalculationService(implicit mongo: MongoBase, scheduler: Scheduler): Task[CalculationService] =
+    for {
+      mvar <- MVar.of[Task, Map[String, CacheEntry]](Map())
+    } yield new CalculationService(mvar)
 }
